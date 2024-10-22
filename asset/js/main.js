7f579b99bff2cc80c031ec5cd95cb2604d925d2f
alimustafvi202 <mamustafvi202@gmail.com>
2024-10-22 23:58:12 +0500

    changes

--- asset/js/main.js
+++ asset/js/main.js
@@ -26,3 +26,32 @@ var containerEl = document.querySelector('[data-ref~="mixitup-container"]');
         target: '[data-ref~="mixitup-target"]'
     }
 });
+
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var accordions = document.querySelectorAll(".accordion");
+
+    accordions.forEach(function(accordion) {
+      accordion.querySelector(".accordion-header").addEventListener("click", function() {
+        var body = this.nextElementSibling;
+
+        if (body.style.display === "block") {
+          body.style.display = "none";
+          this.querySelector("span").textContent = "+"; // Change icon to "+"
+        } else {
+          // Hide all other accordion bodies
+          document.querySelectorAll(".accordion-body").forEach(function(body) {
+            body.style.display = "none";
+          });
+          document.querySelectorAll(".accordion-header span").forEach(function(span) {
+            span.textContent = "+";
+          });
+
+          body.style.display = "block";
+          this.querySelector("span").textContent = "-"; // Change icon to "-"
+        }
+      });
+    });
+  });
+
